f4e7d51772ec8de64f2a7fb7a6991fcceb2dea85
ottomicheletti <michelettiotto@gmail.com>
2022-09-09 18:46:02 -0300

    feat(desafio5): retorna o nome, curtidas e vendidos de lanches com curtidas = 36 ou vendidos = 85

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
-db.produtos.find({ vendidos: { $lt: 100, $gt: 50 } }, { nome: 1, vendidos: 1, _id: 0 })
+db.produtos.find({ vendidos: { $lt: 100, $gt: 50 } }, { _id: 0, nome: 1, vendidos: 1 })
   .sort({ vendidos: 1 });
